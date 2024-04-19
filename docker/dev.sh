d1c0c037289197d6d765cc6d5df585ec30c51790
grude013 <grude013@umn.edu>
2024-04-18 21:20:44 -0500

    docker staging

--- docker/dev.sh
+++ docker/dev.sh
@@ -1,6 +1,8 @@
+# Claim ownership to docker - may not be necessary
 sudo chown -R $(whoami) ~/.docker
 
-# Build
-docker build $1 -t omnistudy/api:dev -f docker/Dockerfile.dev .
-# Run
+# Build the container
+docker build -t omnistudy/api:dev -f docker/Dockerfile.dev .
+
+# Run the container
 docker run -dp 3001:3001 --name api --network omninet -v `pwd`:/api omnistudy/api:dev
